822bdab5206d7cad6344faafb8b58e13ae4e5730
Lam Nguyen <thanhlam2034@gmail.com>
2023-05-30 14:44:03 +0700

    commit login and send sms

--- src/main/java/constant/ConstantVariable.java
+++ src/main/java/constant/ConstantVariable.java
@@ -1,7 +1,7 @@
 package constant;
 
 public class ConstantVariable {
-    public static String ENDPOINT_KEYCLOAK_QC = "https://keycloak-qc.vietcap.com.vn";
+    public static String ENDPOINT_KEYCLOAK_QC = "https://xxxx";
     public static String USERNAME = "xxxxx";
     public static String PASSWORD = "xxxx";
 }
